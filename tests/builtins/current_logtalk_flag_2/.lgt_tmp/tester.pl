941a4841415945156bb83a4057a8699ceff25620
Paulo Moura <pmoura@logtalk.org>
2012-11-27 18:18:07 +0000

    Remove wrongly committed file

--- tests/builtins/current_logtalk_flag_2/.lgt_tmp/tester.pl
+++
@@ -1,5 +0,0 @@
-:-(multifile(/('$lgt_loaded_file_',4))).
-:-(dynamic(/('$lgt_loaded_file_',4))).
-:-('$hide'(:(user,/('$lgt_loaded_file_',4)))).
-:('$source_location'('/Users/pmoura/Documents/Logtalk/logtalk3/tests/builtins/current_logtalk_flag_2/tester.lgt',1),'$lgt_loaded_file_'('tester.lgt','/Users/pmoura/Documents/Logtalk/logtalk3/tests/builtins/current_logtalk_flag_2/',[],[])).
-:-(initialization(','(set_logtalk_flag(report,warnings),','(logtalk_load(lgtunit(loader)),','(logtalk_load(tests,'.'(hook(lgtunit),[])),::(tests,run)))))).
