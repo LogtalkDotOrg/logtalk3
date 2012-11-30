b03e678e445641fc6c36895da6db9b187e643396
Paulo Moura <pmoura@logtalk.org>
2012-11-30 01:54:15 +0000

    Update the `cleandist.sh` script for the removal of the `examples/tester.sh` script

--- scripts/cleandist.sh
+++ scripts/cleandist.sh
@@ -37,7 +37,6 @@ find . -name .pl-history -print0 | xargs -0 rm -f
 find . -type f -print0 | xargs -0 chmod 644
 find . -type d -print0 | xargs -0 chmod 755
 
-chmod a+x examples/*.sh
 chmod a+x integration/*.sh
 chmod a+x manuals/userman/*.sh
 chmod a+x manuals/refman/*.sh
