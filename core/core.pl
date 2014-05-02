12e3f830a9d6a71b7d5241abe1af29f116c035af
Paulo Moura <pmoura@logtalk.org>
2014-05-02 16:12:25 +0100

    Bump version information to beta 4 in preparation for the next release

--- core/core.pl
+++ core/core.pl
@@ -2311,7 +2311,7 @@ current_logtalk_flag(Flag, Value) :-
 % the last argument is an atom: 'aN' for alpha versions, 'bN' for beta versions
 % (with N being a natural number), and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 0, 0, b3)).
+'$lgt_version_data'(logtalk(3, 0, 0, b4)).
 
 
 
