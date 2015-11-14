14cf825822b855df91114343c646e01332d67459
Paulo Moura <pmoura@logtalk.org>
2015-11-14 02:53:17 +0000

    Update version to 3.02.2-rc1 in preparation for the next release

--- core/core.pl
+++ core/core.pl
@@ -2518,7 +2518,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 2, 1, stable)).
+'$lgt_version_data'(logtalk(3, 2, 2, rc1)).
 
 
 
--- scripts/pack/logtalk/prolog/logtalk.pl
+++ scripts/pack/logtalk/prolog/logtalk.pl
@@ -24,7 +24,7 @@
 :- module(logtalk, []).
 
 :-	prolog_load_context(directory, Directory),
-	atom_concat(Directory, '/../logtalk-3.02.1/', Location),
+	atom_concat(Directory, '/../logtalk-3.02.2/', Location),
 	setenv('LOGTALKHOME', Location),
 	setenv('LOGTALKUSER', Location),
-	user:load_files('../logtalk-3.02.1/integration/logtalk_swi.pl').
+	user:load_files('../logtalk-3.02.2/integration/logtalk_swi.pl').
