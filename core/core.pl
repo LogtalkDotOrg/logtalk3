3e44903edfb91b20b0c2aa2afd2316508d2654e5
Paulo Moura <pmoura@logtalk.org>
2016-08-25 21:54:49 -0700

    Also update version reported by the compiler/runtime

--- core/core.pl
+++ core/core.pl
@@ -2903,7 +2903,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 6, 3, rc9)).
+'$lgt_version_data'(logtalk(3, 7, 0, rc1)).
 
 
 
