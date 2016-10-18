a7b8cb63ca3162178ddac4d1eede9f79c21057b1
Paulo Moura <pmoura@logtalk.org>
2016-10-18 11:07:33 +0100

    Update version to 3.08.1-rc1 in preparation for the next release

--- scripts/pack/logtalk/pack.pl
+++ scripts/pack/logtalk/pack.pl
@@ -1,8 +1,8 @@
 name(logtalk).
 title('Logtalk - Object-Oriented Logic Programming Language').
-version('3.08.0').
+version('3.08.1').
 author('Paulo Moura', 'pmoura@logtalk.org').
 packager('Paulo Moura', 'pmoura@logtalk.org').
 maintainer('Paulo Moura', 'pmoura@logtalk.org').
 home('http://logtalk.org/').
-download('http://logtalk.org/files/swi-prolog/packs/logtalk-3.08.0.tgz').
+download('http://logtalk.org/files/swi-prolog/packs/logtalk-3.08.1.tgz').
