a6709ab842fd1902fbdbbb223e599e4009e74751
Paulo Moura <pmoura@logtalk.org>
2017-02-22 14:43:23 +0000

    Reset to `rc2` for merging into master

--- core/core.pl
+++ core/core.pl
@@ -2974,7 +2974,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 9, 3, rc4)).
+'$lgt_version_data'(logtalk(3, 9, 3, rc2)).
 
 
 
