1ac55d038556dbcbc62371c54a13f9ed38e0cc4a
Paulo Moura <pmoura@logtalk.org>
2017-03-03 16:16:26 +0000

    Also updated compiler reported version to 3.10.2-rc1

--- core/core.pl
+++ core/core.pl
@@ -2972,7 +2972,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 10, 1, stable)).
+'$lgt_version_data'(logtalk(3, 10, 2, rc1)).
 
 
 
