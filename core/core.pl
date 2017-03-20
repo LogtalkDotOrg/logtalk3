c2004742c2806ad68a38b0ad6ba8a3df4dae8315
Paulo Moura <pmoura@logtalk.org>
2017-03-20 21:49:51 +0000

    Update version to 3.10.4-rc1 in preparation for the next release

--- core/core.pl
+++ core/core.pl
@@ -2973,7 +2973,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 10, 3, stable)).
+'$lgt_version_data'(logtalk(3, 10, 4, rc1)).
 
 
 
