8161b604c3c6e4e123a6072803482079b9d7aff4
Paulo Moura <pmoura@logtalk.org>
2018-09-05 10:15:49 +0100

    Change version to 3.20.0 stable in preparation for release

--- core/core.pl
+++ core/core.pl
@@ -3380,7 +3380,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 20, 0, b05)).
+'$lgt_version_data'(logtalk(3, 20, 0, stable)).
 
 
 
