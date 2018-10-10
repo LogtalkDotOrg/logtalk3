9715cbdea4dd2b6960bf3dddf0818eed9823da1e
Paulo Moura <pmoura@logtalk.org>
2018-10-10 19:32:36 +0100

    Don't add a "Last updated on" information to every documentation page
    This avoids committing files where only the build date changed.

--- manuals/sources/conf.py
+++ manuals/sources/conf.py
@@ -97,7 +97,7 @@ html_favicon = 'logtalk.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_last_updated_fmt = ''
+# html_last_updated_fmt = ''
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
