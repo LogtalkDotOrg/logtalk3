12fa9811b0c5a8df83b3b611ccbd9df9f807892a
Paulo Moura <pmoura@logtalk.org>
2019-03-02 11:38:36 +0000

    Update version to 3.25.0-b01 in preparation for the next release

--- docs/sources/_conf.py
+++ docs/sources/_conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Paulo Moura'
 author = 'Paulo Moura'
 
 # The short X.Y version
-version = '3.24.0'
+version = '3.25.0'
 # The full version, including alpha/beta/rc tags
-release = u'v3.24.0'
+release = u'v3.25.0-b01'
 
 
 # -- General configuration ---------------------------------------------------
--- manuals/sources/conf.py
+++ manuals/sources/conf.py
@@ -26,9 +26,9 @@ copyright = u'2019, Paulo Moura'
 author = u'Paulo Moura'
 
 # The short X.Y version
-version = u'3.24.0'
+version = u'3.25.0'
 # The full version, including alpha/beta/rc tags
-release = u'v3.24.0'
+release = u'v3.25.0-b01'
 
 
 # -- General configuration ---------------------------------------------------
