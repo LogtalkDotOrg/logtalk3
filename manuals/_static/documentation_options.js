7d28875b0007c763e4d20c4f234d929290e572ea
Paulo Moura <pmoura@logtalk.org>
2019-08-23 12:02:39 +0100

    Restore accidentally deleted handbook files

---
+++ manuals/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v3.29.0-b03',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
