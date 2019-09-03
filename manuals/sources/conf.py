147877adb717b1178d3be4d7b49f6eb7f709ff84
Paulo Moura <pmoura@logtalk.org>
2019-09-03 18:56:23 +0100

    Update version to 3.30.0-b01 in preparation for the next release

--- manuals/sources/conf.py
+++ manuals/sources/conf.py
@@ -26,9 +26,9 @@ copyright = u'2019, Paulo Moura'
 author = u'Paulo Moura'
 
 # The short X.Y version
-version = u'3.29.0'
+version = '3.30.0'
 # The full version, including alpha/beta/rc tags
-release = u'v3.29.0'
+release = u'v3.30.0-b01'
 
 
 # -- General configuration ---------------------------------------------------
--- scripts/pack-experimental/logtalk/pack.pl
+++ scripts/pack-experimental/logtalk/pack.pl
@@ -1,8 +1,8 @@
 name(logtalk).
 title('Logtalk - Object-Oriented Logic Programming Language').
-version('3.29.0').
+version('3.30.0').
 author('Paulo Moura', 'pmoura@logtalk.org').
 packager('Paulo Moura', 'pmoura@logtalk.org').
 maintainer('Paulo Moura', 'pmoura@logtalk.org').
 home('https://logtalk.org/').
-download('https://logtalk.org/files/swi-prolog/packs/logtalk-3.29.0.tgz').
+download('https://logtalk.org/files/swi-prolog/packs/logtalk-3.30.0.tgz').
