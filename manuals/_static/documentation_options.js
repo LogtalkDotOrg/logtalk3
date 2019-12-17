e30a8b20f1576678b0d022a87a6f3edd067e15fd
Paulo Moura <pmoura@logtalk.org>
2019-12-17 23:30:33 +0000

    Rebuild Handbook and API documentation using Sphinx 2.3.0

--- manuals/_static/documentation_options.js
+++ manuals/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: 'v3.34.0-b06',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
