ef2b842a44447f7e466b2b54f9a0fb42b19b3ef8
Paulo Moura <pmoura@logtalk.org>
2020-03-03 18:56:07 +0000

    Update version to 3.37.0-b01 in preparation for the next release

--- manuals/_static/documentation_options.js
+++ manuals/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v3.36.0',
+    VERSION: 'v3.37.0-b01',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
