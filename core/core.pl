8eac91c60735a51813f550da4f35ebc9e66de894
Paulo Moura <pmoura@logtalk.org>
2020-06-17 09:26:56 +0100

    Change version to 3.39.0 stable in preparation for release

--- core/core.pl
+++ core/core.pl
@@ -3464,7 +3464,7 @@ create_logtalk_flag(Flag, Value, Options) :-
 % versions, 'rcN' for release candidates (with N being a natural number),
 % and 'stable' for stable versions
 
-'$lgt_version_data'(logtalk(3, 39, 0, b05)).
+'$lgt_version_data'(logtalk(3, 39, 0, stable)).
 
 
 
