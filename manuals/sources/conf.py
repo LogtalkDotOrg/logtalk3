ee619b31641cdb1f74e35558e906915f370932db
Paulo Moura <pmoura@logtalk.org>
2021-03-24 09:46:56 +0000

    Update version to 3.46.0-b01 in preparation for the next release

--- manuals/sources/conf.py
+++ manuals/sources/conf.py
@@ -26,9 +26,9 @@ copyright = u'1998-2021, Paulo Moura'
 author = u'Paulo Moura'
 
 # The short X.Y version
-version = '3.45.0'
+version = '3.46.0'
 # The full version, including alpha/beta/rc tags
-release = u'v3.45.0'
+release = u'v3.46.0-b01'
 
 
 # -- General configuration ---------------------------------------------------
