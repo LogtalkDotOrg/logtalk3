5c6767a6037fee65e4128b66a76e38a534791112
Paulo Moura <pmoura@logtalk.org>
2022-06-09 00:11:40 +0100

    Change version to 3.56.0 stable in preparation for release

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v3.56.0-b10',
+    VERSION: 'v3.56.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
