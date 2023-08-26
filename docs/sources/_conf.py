a3795443e7b26b8b804d1042bd6262a93c5fb9c0
Paulo Moura <pmoura@logtalk.org>
2023-08-26 08:54:55 +0100

    Update version to 3.70.0-b01 in preparation for the next release

--- docs/sources/_conf.py
+++ docs/sources/_conf.py
@@ -25,9 +25,9 @@ copyright = '1998-2023, Paulo Moura'
 author = 'Paulo Moura'
 
 # The short X.Y version
-version = '3.69.0'
+version = '3.70.0'
 # The full version, including alpha/beta/rc tags
-release = u'v3.69.0'
+release = u'v3.70.0-b01'
 
 
 # -- General configuration ---------------------------------------------------
