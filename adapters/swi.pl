5e04067a7853eafc7e09f2b4f93bfe0d57cca64d
Paulo Moura <pmoura@logtalk.org>
2024-06-03 21:49:13 +0100

    Update SWI-Prolog adapter file support for using tabling in dynamically created objects

--- adapters/swi.pl
+++ adapters/swi.pl
@@ -209,11 +209,15 @@
 '$lgt_prolog_meta_predicate'(load_files(_), load_files(*), predicate).
 '$lgt_prolog_meta_predicate'(load_files(_, _), load_files(*, *), predicate).
 '$lgt_prolog_meta_predicate'(op(_, _, _), op(*, *, *), predicate).
-'$lgt_prolog_meta_predicate'(table(_), table(/), predicate).
+:- if(predicate_property(table(_), built_in)).
+	'$lgt_prolog_meta_predicate'(table(_), table(/), predicate).
+:- endif.
 :- if(predicate_property(tnot(_), built_in)).
 	'$lgt_prolog_meta_predicate'(tnot(_), tnot(0), predicate).
 :- endif.
-'$lgt_prolog_meta_predicate'(untable(_), untable(/), predicate).
+:- if(predicate_property(untable(_), built_in)).
+	'$lgt_prolog_meta_predicate'(untable(_), untable(/), predicate).
+:- endif.
 '$lgt_prolog_meta_predicate'(use_module(_), use_module(*), predicate).
 '$lgt_prolog_meta_predicate'(use_module(_, _), use_module(*, *), predicate).
 
@@ -224,7 +228,9 @@
 '$lgt_prolog_meta_directive'(format_predicate(_, _), format_predicate(*, 0)).
 '$lgt_prolog_meta_directive'(initialization(_, _), initialization(0, *)).
 '$lgt_prolog_meta_directive'(noprofile(_), noprofile(/)).
-'$lgt_prolog_meta_directive'(table(_), table(/)).
+:- if(predicate_property(table(_), built_in)).
+	'$lgt_prolog_meta_directive'(table(_), table(/)).
+:- endif.
 '$lgt_prolog_meta_directive'(thread_initialization(_), thread_initialization(0)).
 '$lgt_prolog_meta_directive'(thread_local(_), thread_local(/)) :-
 	logtalk_load_context(entity_type, Type),
