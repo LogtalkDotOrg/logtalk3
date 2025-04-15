b1c8f4222c0adfb25860dfb56546f60e11df061d
Paulo Moura <pmoura@logtalk.org>
2025-04-15 15:30:31 +0100

    Change version to 3.91.1 stable in preparation for release

--- manuals/sources/conf.py
+++ manuals/sources/conf.py
@@ -26,9 +26,9 @@ copyright = u'1998-2025, Paulo Moura'
 author = u'Paulo Moura'
 
 # The short X.Y version
-version = '3.92.0'
+version = '3.91.1'
 # The full version, including alpha/beta/rc tags
-release = u'v3.92.0-b01'
+release = u'v3.91.1'
 
 
 # -- General configuration ---------------------------------------------------
