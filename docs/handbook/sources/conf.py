5c64fccd7a6066163be4f0908e47941a23a24262
Paulo Moura <pmoura@logtalk.org>
2026-09-07 16:28:55 +0100

    Revert unnecessary change to the Handbook generation setup

--- docs/handbook/sources/conf.py
+++ docs/handbook/sources/conf.py
@@ -147,7 +147,6 @@ latex_elements = {
         \usepackage[defaultsans]{lato}
         \usepackage{inconsolata}
         \usepackage[utf8]{inputenc}
-        \usepackage[T1]{fontenc}
         \DeclareUnicodeCharacter{03C9}{$\omega$}
     ''',
 
