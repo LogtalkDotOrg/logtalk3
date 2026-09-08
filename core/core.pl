42b7cc1398bcc26f2384c34adbb81c499982a856
Paulo Moura <pmoura@logtalk.org>
2026-09-08 15:05:31 +0100

    Fix regression for some linter warning in the previous commit

--- core/core.pl
+++ core/core.pl
@@ -13818,6 +13818,20 @@ create_logtalk_flag(Flag, Value, Options) :-
 
 '$lgt_compile_body'((If -> _; _), _, _, _, Ctx) :-
 	nonvar(If),
+	once((If = (Term1 = Term2); If = (Term1 \= Term2))),
+	once((number(Term1); number(Term2))),
+	'$lgt_comp_ctx_mode'(Ctx, compile(_,_,_)),
+	'$lgt_compiler_flag'(suspicious_calls, warning),
+	'$lgt_source_file_context'(File, Lines, Type, Entity),
+	'$lgt_increment_compiling_warnings_counter',
+	'$lgt_print_message'(
+		warning(arithmetic_expressions),
+		suspicious_call(File, Lines, Type, Entity, If, reason(comparing_numbers_using_unification))
+	),
+	fail.
+
+'$lgt_compile_body'((If -> _; _), _, _, _, Ctx) :-
+	nonvar(If),
 	'$lgt_comp_ctx_mode'(Ctx, compile(_,_,_)),
 	'$lgt_compiler_flag'(conditionals, warning),
 	(	If == ! ->
@@ -14412,18 +14426,6 @@ create_logtalk_flag(Flag, Value, Options) :-
 	fail.
 
 % unification (only lint warnings)
-
-'$lgt_compile_body'(Term1 = Term2, _, _, _, Ctx) :-
-	once((number(Term1); number(Term2))),
-	'$lgt_comp_ctx_mode'(Ctx, compile(_,_,_)),
-	'$lgt_compiler_flag'(arithmetic_expressions, warning),
-	'$lgt_source_file_context'(File, Lines, Type, Entity),
-	'$lgt_increment_compiling_warnings_counter',
-	'$lgt_print_message'(
-		warning(arithmetic_expressions),
-		suspicious_call(File, Lines, Type, Entity, Term1 = Term2, reason(comparing_numbers_using_unification))
-	),
-	fail.
 
 '$lgt_compile_body'(Term1 = Term2, _, _, _, Ctx) :-
 	'$lgt_comp_ctx_mode'(Ctx, compile(_,_,_)),
